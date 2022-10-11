26354c7339e7ff69d1dd87253a683a8b5deb9ff6
Jorge Lucas <37757146+thepinkwheat@users.noreply.github.com>
2022-10-11 17:11:06 -0300

    Node+EJS - Segunda parte - Discover

--- server.js
+++ server.js
@@ -4,11 +4,41 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", function(req, res) {
-  res.render("index")
+  const items = [
+    {
+      title:"D",
+      message:"Desenvolver aplicações/serviços de forma fácil"
+    },
+    {
+      title:"E",
+      message:"EJS usa Js para renderizar HTML"
+    },
+    {
+      title:"M",
+      message:"Muito fácil de usar"
+    },
+    {
+      title:"A",
+      message:"A mais prática forma de criar serviços"
+    },
+    {
+      title:"I",
+      message:"Infinidade de utilidades"
+    },
+    {
+      title:"S",
+      message:"Sintaxe simples"
+    }
+  ]
+
+  res.render("pages/index", {
+    qualitys: items,
+  })
 })
 
 app.get("/sobre", function(req, res) {
-  res.render("about")
+  res.render("pages/about")
 })
 
-app.listen(8080)
+app.listen(8080);
+console.log('Rodando')
